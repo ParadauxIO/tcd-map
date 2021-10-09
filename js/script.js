567dbe7e018c06a34a64480f45836b76eb9a7bb9
Rían Errity <rian@paradaux.io>
2021-10-09 21:38:23 +0100

    Convert to more idiomatic js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,7 @@
-let corner1 = L.latLng(53.345806, -6.259954);
-let corner2 = L.latLng(53.341533, -6.249332);
-let mybounds = L.latLngBounds(corner1, corner2);
-
-var mymap = L.map('mapid').fitBounds(mybounds).setMaxBounds([
-    [53.345806, -6.259954],
-    [53.341533, -6.249332]
-]);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 20,
-    minZoom: 17,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-}).addTo(mymap);
-
-function onMapClick(e) {
-    document.getElementById("click-coords-display").innerHTML = e.latlng;
-}
-
-mymap.on('click', onMapClick);
-
-var buildings = {
+const corner1 = L.latLng(53.345806, -6.259954);
+const corner2 = L.latLng(53.341533, -6.249332);
+const mybounds = L.latLngBounds(corner1, corner2);
+let buildings = [{
     "type": "Feature",
     "properties": {
         "name": "Arts Bloc",
@@ -46,61 +24,58 @@ var buildings = {
             ]
         ]
     }
-};
+}];
 
-function style(feature) {
-    return {
-        fillColor: '#800026',
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
+const mymap = L.map('mapid').fitBounds(mybounds).setMaxBounds([
+    [53.345806, -6.259954],
+    [53.341533, -6.249332]
+]);
 
-function whenClicked(_link) {
-    // e = event
-    //console.log(e);
-    window.location.href = feature.properties.link;
-}
+let init = () => {
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 20,
+        minZoom: 17,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+    }).addTo(mymap);
 
-function onEachFeature(feature, layer) {
-    //bind click
-    layer.on('click', function (e) {
-        window.location.href = feature.properties.link;
+    mymap.on('click', (e) => {
+        document.getElementById("click-coords-display").innerHTML = e.latlng;
     });
-    // does this feature have a property named popupContent?
-    // if (feature.properties && feature.properties.popupContent) {
-    //     layer.bindPopup(feature.properties.popupContent);
-    // }
 
-    if (feature.properties && feature.properties.name) {
-        layer.bindPopup(feature.properties.name, {
-            closeButton: false,
-            offset: L.point(0, -20)
-        });
-        layer.on('mouseover', function () {
-            layer.openPopup();
-        });
-        layer.on('mouseout', function () {
-            layer.closePopup();
-        });
-    }
+    L.geoJson(buildings, {
+        style: {
+            fillColor: '#800026',
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+        },
+        onEachFeature(feature, layer) {
+            //bind click
+            layer.on('click', function (e) {
+                window.location.href = feature.properties.link;
+            });
+        
+            if (feature.properties && feature.properties.name) {
+                layer.bindPopup(feature.properties.name, {
+                    closeButton: false,
+                    offset: L.point(0, -20)
+                });
+                layer.on('mouseover', function () {
+                    layer.openPopup();
+                });
+                layer.on('mouseout', function () {
+                    layer.closePopup();
+                });
+            }
+        }
+    
+    }).addTo(mymap);
 }
 
-L.geoJson(buildings, {
-    style: style,
-    onEachFeature: onEachFeature
-
-}).addTo(mymap);
-
-
-// buildings.forEach(function(building) {
-//     var polygon = L.polygon(building.geometry.coordinates, {
-//         weight: 1,
-//         fillOpacity: 0.7,
-//         color: 'white',
-//         dashArray: '3'
-//     }).addTo(map);
-// });
+init();
